213e59337a6d1bb47ee8e797f664d287a90f9a61
ZarandeAmeElgoLLete <almoha014@gmail.com>
2024-08-09 20:23:23 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const body = document.getElementById("");
+
+const makePost = () => {
+    const screenWidth = screen.width;
+    const postInRow = Math.floor(screenWidth/200)
+}
+
+console.log(screen.width);
